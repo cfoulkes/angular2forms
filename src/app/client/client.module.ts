37235c985bea89ca6c87c9e0fc91cf2b90b54a6d
Colin Foulkes <cfoulkes@protegra.com>
2016-10-14 15:32:33 -0500

    initial

---
+++ src/app/client/client.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ClientsComponent }   from './clients.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [ClientsComponent],
+    providers: [],
+})
+export class ClientModule { }
