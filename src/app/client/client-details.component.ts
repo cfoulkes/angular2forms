cf64b8fa94dd53ea8571289d293fec9945e880fd
Colin <cfoulkes@protegra.com>
2016-10-15 08:03:41 -0500

    finished angular.io forms section

--- src/app/client/client-details.component.ts
+++ src/app/client/client-details.component.ts
@@ -9,12 +9,13 @@ export class ClientDetailsComponent implements OnInit {
 
     client = new Client(1, 'Fred', 'Flintstone', 'Q', 1);
     titles = [
-        {id:1, desc: 'Mr.'},
-        {id:2, desc: 'Mrs.'},
-        {id:3, desc: 'Ms.'},
-        {id:4, desc: 'Miss'}
+        { id: 1, desc: 'Mr.' },
+        { id: 2, desc: 'Mrs.' },
+        { id: 3, desc: 'Ms.' },
+        { id: 4, desc: 'Miss' }
     ];
     submitted = false;
+    active = true;
 
     constructor() { }
 
@@ -23,6 +24,22 @@ export class ClientDetailsComponent implements OnInit {
     onSubmit() {
         console.log('onSubmit');
         this.submitted = true;
+    }
+
+    newClient() {
+        this.client = { id: 100, firstName: '', lastName: '', initials: '', titleId: 1 };
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
+    }
+
+    titleForId(id): String {
+        var titleStr = '';
+        this.titles.forEach(function(title) {
+            if (title.id === id) {
+                titleStr = title.desc;
+            }
+        });
+        return titleStr;
     }
 
 }
