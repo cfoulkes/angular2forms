ce592a62436926e95b2c11caf0627e8822b0f86c
Colin Foulkes <cfoulkes@protegra.com>
2016-10-21 10:16:31 -0500

    md hooked up

--- src/app/md-simple/md-simple.component.ts
+++ src/app/md-simple/md-simple.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'md-simple',
-  templateUrl: './md-simple.component.html',
-  styleUrls: ['./md-simple.component.css']
+    selector: 'md-simple',
+    templateUrl: './md-simple.component.html',
+    styleUrls: ['./md-simple.component.css']
 })
 export class MdSimpleComponent implements OnInit {
 
-  constructor() { }
+    theForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.theForm = new FormGroup({
+            firstName: new FormControl(''),
+            lastName: new FormControl(''),
+            address: new FormGroup({
+                street: new FormControl(''),
+                city: new FormControl('')
+            })
+        });
+    }
+
+    onSubmit() {
+        //do something with this.theForm
+        console.log('onSubmit: ' + JSON.stringify(this.theForm.value));
+    }
 
 }
